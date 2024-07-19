a7d4b8e337ad9216f8e202ab7e4ce419b3f630be
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-07-18 19:55:47 -0500

    Se agrega componente Navbar y funcionalidad de ruta activa

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,52 @@
+import { NavLink } from 'react-router-dom';
+
+export const Navbar = () => {
+   // Estilos opción Navbar activo
+   const activeStyle = 'underline underline-offset-4';
+   return (
+      <nav className='fixed z-10 border w-full top-0 py-5 px-4 bg-white'>
+         <ul className='flex items-center gap-3'>
+            <li>
+               <NavLink
+                  to='/'
+                  className={({ isActive }) =>
+                     isActive ? activeStyle : undefined
+                  }
+               >
+                  Shalom
+               </NavLink>
+            </li>
+            <li>
+               <NavLink
+                  to='/miembros'
+                  className={({ isActive }) =>
+                     isActive ? activeStyle : undefined
+                  }
+               >
+                  Miembros
+               </NavLink>
+            </li>
+            <li>
+               <NavLink
+                  to='/ministerios'
+                  className={({ isActive }) =>
+                     isActive ? activeStyle : undefined
+                  }
+               >
+                  Ministerios
+               </NavLink>
+            </li>
+            <li>
+               <NavLink
+                  to='/miem_mini'
+                  className={({ isActive }) =>
+                     isActive ? activeStyle : undefined
+                  }
+               >
+                  Miem/Minis
+               </NavLink>
+            </li>
+         </ul>
+      </nav>
+   );
+};
