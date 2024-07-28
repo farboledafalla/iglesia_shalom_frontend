d708379eeecc26d56dd46770dbdb402af0f1efdc
Franklim Arboleda Falla <franklim.arboleda@gmail.com>
2024-07-28 11:48:54 -0500

    Se refactoriza el estado global dejando algunos estado locales en el componente FilaMiembro

--- src/components/Miembros/FilaMiembro.jsx
+++ src/components/Miembros/FilaMiembro.jsx
@@ -1,5 +1,5 @@
 // Componentes React
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Librerias
 import PropTypes from 'prop-types';
@@ -8,57 +8,39 @@ import { Dialog } from '@material-ui/core';
 
 export const FilaMiembro = ({ miembro, onUpdateMiembro, onDeleteMiembro }) => {
    // Estados
+   const [localMiembro, setLocalMiembro] = useState(miembro);
    const [edit, setEdit] = useState(false);
    const [openDialog, setOpenDialog] = useState(false);
-   const [infoNuevoMiembro, setInfoNuevoMiembro] = useState({
-      id_miembro: miembro.id_miembro,
-      cedula: miembro.cedula,
-      nombres: miembro.nombres,
-      apellidos: miembro.apellidos,
-      celular: miembro.celular,
-   });
+
+   useEffect(() => {
+      setLocalMiembro(miembro);
+   }, [miembro]);
 
    // Cambia con el botón editar
-   const onEditMiembro = () => {
-      console.log('id_miembro: ', infoNuevoMiembro.id_miembro);
+   const handleEditToggle = () => {
       setEdit(!edit);
    };
 
    // Guardar los cambios
-   const onUpdatePropiedadCedula = (event) => {
-      setInfoNuevoMiembro({
-         ...infoNuevoMiembro,
-         cedula: event.target.value,
-      });
-   };
-   const onUpdatePropiedadNombres = (event) => {
-      setInfoNuevoMiembro({
-         ...infoNuevoMiembro,
-         nombres: event.target.value,
-      });
-   };
-   const onUpdatePropiedadApellidos = (event) => {
-      setInfoNuevoMiembro({
-         ...infoNuevoMiembro,
-         apellidos: event.target.value,
-      });
-   };
-   const onUpdatePropiedadCelular = (event) => {
-      setInfoNuevoMiembro({
-         ...infoNuevoMiembro,
-         celular: event.target.value,
+   const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setLocalMiembro({
+         ...localMiembro,
+         [name]: value,
       });
    };
 
    // Enviar al padre objeto editado
-   const onUpdateObjetoMiembro = () => {
-      onUpdateMiembro(infoNuevoMiembro);
+   const handleUpdate = () => {
+      onUpdateMiembro(localMiembro);
       setEdit(false);
    };
 
    // Enviar al padre objeto para eliminar
-   const onDeleteObjetoMiembro = () => {
-      onDeleteMiembro(infoNuevoMiembro);
+   const handleDelete = () => {
+      onDeleteMiembro(localMiembro);
+      // Para que no se vuelva a mostrar cuando elimina un registro
+      setOpenDialog(false);
    };
 
    return (
@@ -66,35 +48,39 @@ export const FilaMiembro = ({ miembro, onUpdateMiembro, onDeleteMiembro }) => {
          {edit ? (
             <>
                <td>
-                  <input type='hidden' value={infoNuevoMiembro.id_miembro} />
+                  <input type='hidden' value={localMiembro.id_miembro} />
                   <input
                      type='number'
-                     value={infoNuevoMiembro.cedula}
-                     onChange={onUpdatePropiedadCedula}
+                     name='cedula'
+                     value={localMiembro.cedula}
+                     onChange={handleInputChange}
                      className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
                   />
                </td>
                <td>
                   <input
                      type='text'
-                     value={infoNuevoMiembro.nombres}
-                     onChange={onUpdatePropiedadNombres}
+                     name='nombres'
+                     value={localMiembro.nombres}
+                     onChange={handleInputChange}
                      className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
                   />
                </td>
                <td>
                   <input
                      type='text'
-                     value={infoNuevoMiembro.apellidos}
-                     onChange={onUpdatePropiedadApellidos}
+                     name='apellidos'
+                     value={localMiembro.apellidos}
+                     onChange={handleInputChange}
                      className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
                   />
                </td>
                <td>
                   <input
                      type='number'
-                     value={infoNuevoMiembro.celular}
-                     onChange={onUpdatePropiedadCelular}
+                     name='celular'
+                     value={localMiembro.celular}
+                     onChange={handleInputChange}
                      className='border border-gray-500 p-2 mt-2 mb-3 rounded-lg focus:outline-none focus:border-indigo-500'
                   />
                </td>
@@ -113,13 +99,13 @@ export const FilaMiembro = ({ miembro, onUpdateMiembro, onDeleteMiembro }) => {
                <>
                   <Tooltip title='Confirmar Edición' placement='top-end' arrow>
                      <i
-                        onClick={onUpdateObjetoMiembro}
+                        onClick={handleUpdate}
                         className='fas fa-check text-green-500 mx-2 cursor-pointer'
                      />
                   </Tooltip>
-                  <Tooltip title='Candelar Edición' placement='top-start' arrow>
+                  <Tooltip title='Cancelar Edición' placement='top-start' arrow>
                      <i
-                        onClick={onEditMiembro}
+                        onClick={handleEditToggle}
                         className='fas fa-ban text-red-500 mx-2 cursor-pointer'
                      />
                   </Tooltip>
@@ -128,7 +114,7 @@ export const FilaMiembro = ({ miembro, onUpdateMiembro, onDeleteMiembro }) => {
                <>
                   <Tooltip title='Editar Miembro' placement='top-end' arrow>
                      <i
-                        onClick={onEditMiembro}
+                        onClick={handleEditToggle}
                         className='fas fa-pencil-alt text-yellow-500 mx-2 cursor-pointer'
                      />
                   </Tooltip>
@@ -147,7 +133,7 @@ export const FilaMiembro = ({ miembro, onUpdateMiembro, onDeleteMiembro }) => {
                   </h1>
                   <div className='flex w-full items-center justify-center my-4'>
                      <button
-                        onClick={onDeleteObjetoMiembro}
+                        onClick={handleDelete}
                         className='mx-2 px-4 py-2 bg-green-500 text-white hover:bg-green-700 rounded-md shadow-md'
                      >
                         Si
